79e923115e4c9072154b2af82bdab05da8e6874e
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2025-11-09 23:11:00 +0900

    init

---
+++ src/main/java/com/example/scalableservice/ScalableServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.scalableservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalableServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scalableservice/ScalableServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scalableservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalableServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
