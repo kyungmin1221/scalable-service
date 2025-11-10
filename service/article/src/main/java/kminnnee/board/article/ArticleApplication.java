af973bd8a6b8931e75a3b008882c63d598ddd33d
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2025-11-10 20:50:44 +0900

    Feat : Project Basic Setting

---
+++ service/article/src/main/java/kminnnee/board/article/ArticleApplication.java
@@ -0,0 +1,11 @@
+package kminnnee.board.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+}
